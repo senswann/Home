426e896f908982331dd2823423c45c992f3111d3
senswann <84702005+senswann@users.noreply.github.com>
2024-06-07 15:00:12 +0200

    add : background change system

--- project/TestSite/app.js
+++ project/TestSite/app.js
@@ -3,6 +3,7 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry);
         if(entry.isIntersecting){
             entry.target.classList.add('show');
+            document.body.style.backgroundColor = getComputedStyle(entry.target).getPropertyValue('--bg-color');
         } else{
             entry.target.classList.remove('show');
         }
